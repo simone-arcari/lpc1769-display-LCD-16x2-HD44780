363380661468f17ae877c0820d6a798165b60458
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2021-03-21 14:08:52 +0100

    Update lpc1769_HD44780.c

--- lpc1769_HD44780.c
+++ lpc1769_HD44780.c
@@ -26,18 +26,18 @@ int main(void) {
 	lcd_pin(13, 28, 27, 22, 21, 3, 2, two, zero, zero, zero, zero, zero, zero);
 	init_LCD();
 
-	putCommand(DISPLAY_CLEAR_CMD);          					// clear display
-	setCursor(0,0);    											// first line, first column, 1st position
-	writeString((unsigned char*)"Display LCD 4bit", false);		// message
+	putCommand(DISPLAY_CLEAR_CMD);          		 // clear display
+	setCursor(0,0);    				         // first line, first column, 1st position
+	writeString((unsigned char*)"Display LCD 4bit", false);  // message
 
 	delayMs(1000);
 
     while(1) {
     	static int number = 0;
 
-    	setCursor(1,0);    										// second line, fist column
-    	writeString((unsigned char*)"Count: ", false);			// message
-    	writeNumber(number);									// send number
+    	setCursor(1,0);    				 // second line, fist column
+    	writeString((unsigned char*)"Count: ", false);	 // message
+    	writeNumber(number);				 // send number
 
     	number++;
 
