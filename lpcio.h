d2a1043924940e0fb639e9b2b30fde3de93d8251
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2021-03-21 14:14:18 +0100

    Update lpcio.h

--- lpcio.h
+++ lpcio.h
@@ -12,10 +12,10 @@ typedef enum {OUTPUT, INPUT} pin_status;
 typedef enum {HIGH, LOW} pin_value;
 typedef enum {zero, one, two, three} pin_port;
 
-void pinMode(int pin, pin_status status, pin_port port);		// function to set the pin state
+void pinMode(int pin, pin_status status, pin_port port);		  // function to set the pin state
 void digitalWrite(int pin, pin_value value,  pin_port port);	// function to set the pin value(0-1)
-void delayMs(unsigned short delayInMs);							// delay function in milliseconds
-void delayUs(unsigned short delayInUs);							// delay function in microseconds
+void delayMs(unsigned short delayInMs);							          // delay function in milliseconds
+void delayUs(unsigned short delayInUs);							          // delay function in microseconds
 
 
 #endif /* LPCIO_H_ */
